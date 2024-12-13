7e1b0efe9d26b2be505599a60fe8e7db980991d3
sumito tsukada <tsukada@sumito.jp>
2024-12-14 01:12:12 +0900

    101

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
-import {SiteConfig} from "@/types";
+import { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
-    name: "Post Writer",
-    description: "blog投稿ができる webアプリケーション",
-    url: "https://localhost:3000",
-    ogImage: "",
-    links: {
-        x: "https://twitter.com/19840209",
-        github: "https://github.com/GitSumito",
-    },
-};
+  name: 'Post Writer',
+  description: 'blog投稿ができる webアプリケーション',
+  url: 'https://post-writer-zeta.vercel.app/',
+  ogImage: '',
+  links: {
+    x: 'https://twitter.com/19840209',
+    github: 'https://github.com/GitSumito',
+  },
+}
